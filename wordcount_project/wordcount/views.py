2581077f759f899d5bb6b5613a6d8bc006030a8d
brianbirir <brianbirir@gmail.com>
2019-04-01 16:41:12 +0300

    Add template folder and new views

---
+++ wordcount_project/wordcount/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html', {'hi_there': 'This is Brian'})
+
+
+def eggs(request):
+    return HttpResponse('Eggs are tasty when scrambled')
