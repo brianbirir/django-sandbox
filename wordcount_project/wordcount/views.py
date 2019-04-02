6e6ee4dd882125301d2e6ecb6347a02171f660a1
brianbirir <brianbirir@gmail.com>
2019-04-02 11:48:12 +0300

    Add count function
    - count number of times a word appears and sort the result in descending
    order using word

--- wordcount_project/wordcount/views.py
+++ wordcount_project/wordcount/views.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -11,5 +12,24 @@ def eggs(request):
 
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    word_dict = {}
     
+    for word in wordlist:
+        if word in word_dict:
+            # increase count if word is found in dictionary
+            word_dict[word] += 1
+        else:
+            # add initial count of one to the dictionary
+            word_dict[word] = 1
+    
+    sorted_words = sorted(word_dict.items(), reverse=True, key=itemgetter(1))
+    
+    return render(request,\
+            'count.html',\
+            {'fulltext': fulltext, 
+            'count': len(wordlist), 
+            'sorted_words': sorted_words
+            }\
+        )
